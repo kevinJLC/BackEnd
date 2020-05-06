44db0e98fcf5471e7d291f5e412e121761165ab3
{{kevin.name}} <lopezcarrillokevin36@gmail.com>
2020-05-05 20:59:07 -0500

    .

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const productos=require('../Controllers/productos.controller')
 
-router.get('/',)
+router.get('/',productos.getBienvenida)
 router.get('/api/productos/insertar/info',productos.infoPostProducto)
 router.post('/api/productos/insertar',productos.PostProducto)
 router.get('/api/productos/consulta',productos.getProducto)
